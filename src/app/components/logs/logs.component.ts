644a89b79e617ad4d874b787408353681aee4c28
helen-tan <helentanmz0909@gmail.com>
2023-05-05 16:55:43 +0800

    Dynamically display logs data

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
-export class LogsComponent {
+export class LogsComponent implements OnInit {
+  logs!: {
+    id: string,
+    text: string,
+    date: any
+  }[];
 
+  constructor() {}
+
+  ngOnInit() {
+    this.logs = [
+      { id: "1", text: "Generated components", date: new Date('12/26/2023 12:54:23') },
+      { id: "2", text: "Added Bootstrap", date: new Date('12/27/2023 9:33:23') },
+      { id: "3", text: "Added logs component", date: new Date('12/27/2023 12:54:23') }
+    ]
+  }
 }
