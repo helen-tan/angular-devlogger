fb5e1ff201e464d7b591343d947179789715fab1
helen tan <helentanmz0909@gmail.com>
2023-05-11 16:42:23 +0800

    Display message for when there are no logs

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -17,11 +17,12 @@ export class LogService {
   stateClear = this.stateSource.asObservable();
 
   constructor() {
-    this.logs = [
-      { id: "1", text: "Generated components", date: new Date('12/26/2023 12:54:23') },
-      { id: "2", text: "Added Bootstrap", date: new Date('12/27/2023 9:33:23') },
-      { id: "3", text: "Added logs component", date: new Date('12/27/2023 12:54:23') }
-    ]
+    // this.logs = [
+    //   { id: "1", text: "Generated components", date: new Date('12/26/2023 12:54:23') },
+    //   { id: "2", text: "Added Bootstrap", date: new Date('12/27/2023 9:33:23') },
+    //   { id: "3", text: "Added logs component", date: new Date('12/27/2023 12:54:23') }
+    // ]
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
